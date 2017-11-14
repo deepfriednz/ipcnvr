69060b966ef4cc7f9c60ca13a930f437cdc1bf34
deepfriednz <louistan3@gmail.com>
2017-11-14 23:12:53 +1300

    fixed reading into array and now added statement to record via mjpeg and rtsp though the connection strings still need to be put into variables?

--- capture.bash
+++ capture.bash
@@ -11,17 +11,31 @@ campass=$(cat credentials.config | cut -f2 -d:)
 basepath="/data/video"
 cam1="livingroom"
 cam2="frontdoor"
-camlist=$(getCams $camListFile)
+fps="24"
+recordTime="600" #record time in seconds
+readarray -t camlist < $camListFile
 
 camipport1=""
 camipport2=""
 
-timestamp=$(date +%Y-%m-%d_%H.%M)
+timestamp=$(date +%Y-%m-%d)
 
-
+counter=1
 for i in "${camlist[@]}"
 do
-	echo "cam $i"
+	camip=$(echo $i | cut -f1 -d:)
+	camport=$(echo $i | cut -f2 -d:)
+	protocol=$(echo $i | cut -f3 -d:)
+	location=$(echo $i | cut -f4 -d:)
+	echo "Current CAM: $camip:$camport $protocol"
+	timestamp=$(date +%Y-%m-%d)
+	if [ $protocol = "mjpeg" ]
+	then
+		ffmpeg -i "http://$camip:$camport/videostream.cgi?user=$camuser&pwd=$campass" -r $fps -vcodec copy -t $recordTime $basepath/$location-$counter-$timestamp.mp4 2> $basepath/$location-$counter-$timestamp.log &
+	elif [ $proctocol = "rtsp" ] 
+		ffmpeg -i "rtsp://$camuser:$campass@$campip:$camport/cam/realmonitor?channel=1&subtype=0" -r $fps -vcodec copy -an -t $recordTime $basepath/$location-$counter-$timestamp.mp4 2> $basepath/$location-$counter-$timestamp.log &
+	fi
+	counter+=1
 done
 
 #ffmpeg -i rtsp://username:password@192.168.1.201:80/videoMain -r 30 -vcodec copy -an -t 900 $RECpath/cam01/$name.mp4 </dev/null >/dev/null 2>/tmp/cam01.log &
